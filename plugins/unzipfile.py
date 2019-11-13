13059f939ec7c420820cea55ecf5a35828a25d1a
M.Furkan <byazade@yandex.com>
2019-11-13 14:11:21 +0300

    .

--- plugins/unzipfile.py
+++ plugins/unzipfile.py
@@ -4,29 +4,30 @@
 
 # the logging things
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-from zipfile import ZipFile
 import os
 import shutil
 import subprocess
 import time
+from zipfile import ZipFile
+
+import pyrogram
+from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import humanbytes, progress_for_pyrogram
+# the Strings used for this "thing"
+from translation import Translation
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from sample_config import Config
 
-# the Strings used for this "thing"
-from translation import Translation
 
-import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 
 
 extracted = Config.DOWNLOAD_LOCATION + "extracted/"
