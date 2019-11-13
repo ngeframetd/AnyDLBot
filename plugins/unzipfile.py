70487a9d1ce6ccad5ca207f0afc5737277ea7cd6
M.Furkan <byazade@yandex.com>
2019-11-13 14:18:13 +0300

    .

--- plugins/unzipfile.py
+++ plugins/unzipfile.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+""" command: .unzip
+coded by @By_Azade
+code rewritten my SnapDragon7410
+"""
 
-# the logging things
+import asyncio
 import logging
 import os
 import shutil
 import subprocess
 import time
+import zipfile
+from datetime import datetime
 from zipfile import ZipFile
 
 import pyrogram
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import humanbytes, progress_for_pyrogram
 # the Strings used for this "thing"
@@ -19,6 +24,7 @@ from translation import Translation
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
@@ -30,45 +36,47 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 
+
+
 extracted = Config.DOWNLOAD_LOCATION + "extracted/"
 thumb_image_path = Config.DOWNLOAD_LOCATION + "/thumb_image.jpg"
 if not os.path.isdir(extracted):
     os.makedirs(extracted)
 
+
 @pyrogram.Client.on_message(pyrogram.Filters.command(["zipcikar"]))
 async def unzip(bot, update):
-    
-    if update.from_user.id not in Config.AUTH_USERS:
-        
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        if bot.reply_to_msg_id:
-            start = datetime.now()
-            reply_message = await bot.get_reply_message()
-            try:
-                c_time = time.time()
-                downloaded_file_name = await bot.download_media(
-                    reply_message,
-                    Config.TMP_DOWNLOAD_DIRECTORY,
-                    progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                        progress(d, t, bot.edit_message_text("Processing ..."), c_time, "trying to download")
-                    )
+    if bot.fwd_from:
+        return
+    mone = await bot.edit_message_text("Processing ...")
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if bot.reply_to_msg_id:
+        start = datetime.now()
+        reply_message = await bot.get_reply_message()
+        try:
+            c_time = time.time()
+            downloaded_file_name = await borg.download_media(
+                reply_message,
+                Config.DOWNLOAD_LOCATION,
+                progress_callback=lambda d, t: asyncio.get_bot_loop().create_task(
+                    progress(d, t, mone, c_time, "trying to download")
                 )
-            except Exception as e:  # pylint:disable=C0103,W0703
-                await bot.edit_message_text(str(e))
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await mone.edit(str(e))
         else:
             end = datetime.now()
             ms = (end - start).seconds
-            await bot.edit_message_text("Stored the zip to `{}` in {} seconds.".format(downloaded_file_name, ms))    
-        return
-    TRChatBase(update.from_user.id, update.text, "unzip")
-    with zipfile.ZipFile(downloaded_file_name, 'r') as zip_ref:
-        zip_ref.extractall(extracted)
-    filename = sorted(get_lst_of_files(extracted, []))
-    for single_file in filename:
+            await mone.edit("Stored the zip to `{}` in {} seconds.".format(downloaded_file_name, ms))
+
+        with zipfile.ZipFile(downloaded_file_name, 'r') as zip_ref:
+            zip_ref.extractall(extracted)
+        filename = sorted(get_lst_of_files(extracted, []))
+        #filename = filename + "/"
+        await bot.edit_message_text("Unzipping now")
+        # r=root, d=directories, f = files
+        for single_file in filename:
             if os.path.exists(single_file):
                 # https://stackoverflow.com/a/678242/4723940
                 caption_rts = os.path.basename(single_file)
@@ -97,129 +105,42 @@ async def unzip(bot, update):
                             supports_streaming=True
                         )
                     ]
-                    try:
-                        await bot.send_message(
-                            event.chat_id,
-                            single_file,
-                            caption=f"UnZipped `{caption_rts}`",
-                            force_document=force_document,
-                            supports_streaming=supports_streaming,
-                            allow_cache=False,
-                            reply_to=event.message.id,
-                            attributes=document_attributes,
-                            # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                            #     progress(d, t, event, c_time, "trying to upload")
-                            # )
-                        )
-                    except Exception as e:
-                        await bot.send_message(
-                            event.chat_id,
-                            "{} caused `{}`".format(caption_rts, str(e)),
-                            reply_to=event.message.id
-                        )
+                try:
+                    await bot.send_file(
+                        bot.chat_id,
+                        single_file,
+                        caption=f"UnZipped `{caption_rts}`",
+                        force_document=force_document,
+                        supports_streaming=supports_streaming,
+                        allow_cache=False,
+                        reply_to=bot.message.id,
+                        attributes=document_attributes,
+                        # progress_callback=lambda d, t: asyncio.get_bot_loop().create_task(
+                        #     progress(d, t, bot, c_time, "trying to upload")
+                        # )
+                    )
+                except Exception as e:
+                    await bot.send_message(
+                        bot.chat_id,
+                        "{} caused `{}`".format(caption_rts, str(e)),
+                        reply_to=bot.message.id
+                    )
                     # some media were having some issues
                     continue
-                    os.remove(single_file)
-            os.remove(downloaded_file_name)
-    # reply_message = update.reply_to_message
-    # if ((reply_message is not None) and
-    #     (reply_message.document is not None) and
-    #     (reply_message.document.file_name.endswith(Translation.UNZIP_SUPPORTED_EXTENSIONS))):
-    #     a = await bot.send_message(
-    #         chat_id=update.chat.id,
-    #         text=Translation.DOWNLOAD_START,
-    #         reply_to_message_id=update.message_id
-    #     )
-    #     c_time = time.time()
-    #     try:
-    #         await bot.download_media(
-    #             message=reply_message,
-    #             file_name=saved_file_path,
-    #             progress=progress_for_pyrogram,
-    #             progress_args=(
-    #                 Translation.DOWNLOAD_START,
-    #                 a,
-    #                 c_time
-    #             )
-    #         )
-    #     except (ValueError) as e:
-    #         await bot.edit_message_text(
-    #             chat_id=update.chat.id,
-    #             text=str(e),
-    #             message_id=a.message_id
-    #         )
-    #     else:
-    #         await bot.edit_message_text(
-    #             chat_id=update.chat.id,
-    #             text=Translation.SAVED_RECVD_DOC_FILE,
-    #             message_id=a.message_id
-    #         )
-    #         extract_dir_path = extracted
-    #         if not os.path.isdir(extract_dir_path):
-    #             os.makedirs(extract_dir_path)
-    #         await bot.edit_message_text(
-    #             chat_id=update.chat.id,
-    #             text=Translation.EXTRACT_ZIP_INTRO_THREE,
-    #             message_id=a.message_id
-    #         )
-    #         try:
-                
-    #             # https://stackoverflow.com/a/39629367/4723940
-    #             logger.info(command_to_exec)
-    #             t_response = subprocess.check_output(
-    #                 command_to_exec, stderr=subprocess.STDOUT)
-    #             # https://stackoverflow.com/a/26178369/4723940
-    #         except:
-    #             try:
-    #                 os.remove(saved_file_path)
-    #                 shutil.rmtree(extract_dir_path)
-    #             except:
-    #                 pass
-    #             await bot.edit_message_text(
-    #                 chat_id=update.chat.id,
-    #                 text=Translation.EXTRACT_ZIP_ERRS_OCCURED,
-    #                 disable_web_page_preview=True,
-    #                 parse_mode="html",
-    #                 message_id=a.message_id
-    #             )
-    #         else:
-    #             os.remove(saved_file_path)
-    #             inline_keyboard = []
-    #             zip_file_contents = os.listdir(extract_dir_path)
-    #             i = 0
-    #             for current_file in zip_file_contents:
-    #                 cb_string = "ZIP:{}:ZIP".format(str(i))
-    #                 inline_keyboard.append([
-    #                     pyrogram.InlineKeyboardButton(
-    #                         current_file,
-    #                         callback_data=cb_string.encode("UTF-8")
-    #                     )
-    #                 ])
-    #                 i = i + 1
-    #             cb_string = "ZIP:{}:ZIP".format("ALL")
-    #             inline_keyboard.append([
-    #                 pyrogram.InlineKeyboardButton(
-    #                     "Upload All Files",
-    #                     callback_data=cb_string.encode("UTF-8")
-    #                 )
-    #             ])
-    #             cb_string = "ZIP:{}:ZIP".format("NONE")
-    #             inline_keyboard.append([
-    #                 pyrogram.InlineKeyboardButton(
-    #                     "Cancel",
-    #                     callback_data=cb_string.encode("UTF-8")
-    #                 )
-    #             ])
-    #             reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
-    #             await bot.edit_message_text(
-    #                 chat_id=update.chat.id,
-    #                 text=Translation.EXTRACT_ZIP_STEP_TWO,
-    #                 message_id=a.message_id,
-    #                 reply_markup=reply_markup,
-    #             )
-    # else:
-    #     await bot.send_message(
-    #         chat_id=update.chat.id,
-    #         text=Translation.EXTRACT_ZIP_INTRO_ONE,
-    #         reply_to_message_id=update.message_id
-    #     )
+                os.remove(single_file)
+        os.remove(downloaded_file_name)
+
+
+
+
+
+
+
+def get_lst_of_files(input_directory, output_lst):
+    filesinfolder = os.listdir(input_directory)
+    for file_name in filesinfolder:
+        current_file_name = os.path.join(input_directory, file_name)
+        if os.path.isdir(current_file_name):
+            return get_lst_of_files(current_file_name, output_lst)
+        output_lst.append(current_file_name)
+    return output_lst
