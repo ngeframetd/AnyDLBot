415e6a0a07dd4fcf7a711f6cb6e56e4be51a8068
M.Furkan <byazade@yandex.com>
2019-11-13 19:02:26 +0300

    ü

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -89,7 +89,7 @@ async def unzip(bot, update):
                 command_to_exec = [
                     "7z",
                     "e",
-                    "-o" + extract_dir_path,
+                    "-o", + extract_dir_path,
                     saved_file_path
                 ]
                 # https://stackoverflow.com/a/39629367/4723940
