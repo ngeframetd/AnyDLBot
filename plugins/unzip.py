ead6381369a77d02567b7431c6c7f2e78cbf0d5f
M.Furkan <byazade@yandex.com>
2020-01-08 13:13:22 +0300

    .

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -89,9 +89,10 @@ async def unzip(bot, update):
                 command_to_exec = [
                     "unzip",
                     "-o",
-                    extract_dir_path,
+                    saved_file_path,
+                    # extract_dir_path,
                     "-d",
-                    saved_file_path
+                    extract_dir_path
                 ]
                 # https://stackoverflow.com/a/39629367/4723940
                 logger.info(command_to_exec)
