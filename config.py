9f8bbf9ecde1a8fdee0a59b09804073657996ce5
ngeframetd <70860536+ngeframetd@users.noreply.github.com>
2020-11-28 16:24:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,3 @@ class Development(Config):
   APP_ID = 2351562
   API_HASH = "060a6972595e5f208ef04e2b5de47e6e"
   TG_BOT_TOKEN = "1290142463:AAH0gVKlD-fLyaOy-pJ9r-Yu_pPqxHyL9qk"
-  AUTH_USERS = [
-    357990109
-  ]
