d6eb1843a09e908756995b58cc96f7176f763649
ngeframetd <70860536+ngeframetd@users.noreply.github.com>
2020-11-28 15:30:43 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from anydlbot.sample_config import Config
+
+class Development(Config):
+  APP_ID = 2351562
+  API_HASH = "060a6972595e5f208ef04e2b5de47e6e"
+  TG_BOT_TOKEN = "1290142463:AAH0gVKlD-fLyaOy-pJ9r-Yu_pPqxHyL9qk"
+  AUTH_USERS = [
+    357990109
+    975409386
+    1088156168
+    1492632457
+  ]
